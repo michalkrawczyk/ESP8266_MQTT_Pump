2da9875f698032bd3b3adc0c58d48286ea2481b3
mkrwczyk.1@gmail.com <mkrwczyk.1@gmail.com>
2022-08-08 20:49:24 +0200

    new

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+#include "Pump.hpp"
+
+#define CONN_LED D4
+
+water_pump::PumpController pump(D1, 170, 255); // 8-12V Range for 12V Pump
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  delay(10);
+  Serial.println("Start");
+  pump.initPump();
+}
+
+void loop() {
+  // digitalWrite(MCU_LED, HIGH);
+  // delay(1000);
+  // digitalWrite(MCU_LED, LOW);
+  // delay(1000);
+  // put your main code here, to run repeatedly:
+}
