88df8fc991299844c2e62b51f0ed3625ead17d52
mkrwczyk.1@gmail.com <mkrwczyk.1@gmail.com>
2022-11-12 21:45:05 +0100

    small changes in time calculation

--- lib/Utils/MessageProcessing.cpp
+++ lib/Utils/MessageProcessing.cpp
@@ -4,8 +4,13 @@
 
 namespace msg_processing
 {
-  uint64_t calculateTimeMs(uint8_t h, uint8_t m, uint8_t s)
+  uint64_t calculateTimeMs(const uint8_t h, const uint8_t m, const uint8_t s)
   {
+    /** @brief  Calculates time in miliseconds for timed functions
+    * @param h - time in hours
+    * @param m - time in minutes
+    * @param s - time in seconds
+    */
     uint64_t total_time {0};
 
     total_time += h * 3600000;
--- lib/Utils/MessageProcessing.hpp
+++ lib/Utils/MessageProcessing.hpp
@@ -6,7 +6,7 @@
 namespace msg_processing
 {
     // struct Message;
-    uint64_t calculateTimeMs(uint8_t h = 0, uint8_t m = 0, uint8_t s = 0);
+    uint64_t calculateTimeMs(const uint8_t h = 0, const uint8_t m = 0, const uint8_t s = 0);
 }
 
 // struct msg_processing::Message
